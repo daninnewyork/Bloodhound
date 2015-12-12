6380780f1c171652fbe4fbbc3d471ef8dffcb246
Dan Barnes <ihateregistration@gmail.com>
2015-12-12 15:17:39 -0500

    Repository moved

--- test/adapter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* jshint -W079 */
-/* jshint node: true */
-var Promise = require('../app/src/Promise');
-
-exports.resolved = Promise.resolve;
-exports.rejected = Promise.reject;
-exports.deferred = Promise.defer;
